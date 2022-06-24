03e0933343cf9d5cf62744cb470adf66fd6e50fc
cem-sucu <cems.cems.17@gmail.com>
2022-06-24 10:48:47 +0200

    rendu dynamique des pays classé par continent et par nombre de populations

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -4,6 +4,10 @@ import Card from "./Card";
 
 const Countries = () => {
   const [data, setData] = useState([]);
+  const [rangeValue, setRangeValue] = useState(36);
+  const [selectedRadio, setSelectedRadio] = useState("");
+  const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
+
   // les useEffect se joue lorsque les composant est monté
   useEffect(() => {
     axios
@@ -13,13 +17,40 @@ const Countries = () => {
 
   return (
     <div className="countries">
-        <ul className="radio-container">
-            <input type="range" min="1" max="250"/>
-        </ul>
-      <ul>
-        {data.map((country, index) => (
-        <Card key={index} country={country}/>
+      <ul className="radio-container">
+        <input
+          type="range"
+          min="1"
+          max="250"
+          defaultValue={rangeValue}
+          onChange={(e) => setRangeValue(e.target.value)}
+        />
+        {radios.map((continent) => (
+          <li>
+            <input
+              type="radio"
+              id={continent}
+              name="continentRadio"
+             checked={continent === selectedRadio}
+              onChange={(e) => setSelectedRadio(e.target.id)}
+            />
+            <label htmlFor={continent}>{continent}</label>
+          </li>
         ))}
+      </ul>
+      {selectedRadio && (
+        <button onClick={() => setSelectedRadio("")}>
+          Annuler la recherche
+        </button>
+      )}
+      <ul>
+        {data
+          .filter((country) => country.continents[0].includes(selectedRadio))
+          .sort((a, b) => b.population - a.population)
+          .slice(0, rangeValue)
+          .map((country, index) => (
+            <Card key={index} country={country} />
+          ))}
       </ul>
     </div>
   );
